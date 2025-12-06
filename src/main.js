c03f6b2395f5c389581dd089a6b0151565b07e52
Julia <pineapple05955@gmail.com>
2025-12-07 02:07:33 +0300

    ready for yup

--- src/main.js
+++ src/main.js
@@ -1,2 +1,15 @@
+import { object, string, number, date } from 'yup';
+
 
 console.log('Hello world')
+let userSchema = object({
+  name: string().required(),
+  age: number().required().positive().integer(),
+  email: string().email(),
+  website: string().url().nullable(),
+  createdOn: date().default(() => new Date()),
+});
+
+let input = document.getElementById('url-input')
+console.log(input)
+userSchema.validate(input)
